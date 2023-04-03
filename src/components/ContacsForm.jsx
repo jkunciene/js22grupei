4f4ff4600c775d2ef001bd76425a8aae7b1ca508
Mokytojas <Mokytojas@kitm.local>
2023-04-03 11:01:30 +0300

    irasymas i db veikia

--- src/components/ContacsForm.jsx
+++ src/components/ContacsForm.jsx
@@ -1,4 +1,8 @@
-import { useState} from 'react';
+import { useState } from 'react';
+
+import db from '../firebase';
+import { collection, addDoc } from "firebase/firestore"; 
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -9,12 +13,37 @@ const ContacsForm = () => {
 
     console.log(name, email, message)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         //patikrinti ar laukai uzpildyti
+        if (name.length < 3) {
+            alert('Name cant be blank!')
+            return
+        }
+        if (email === "") {
+            alert('Email cant be blank!')
+            return
+        }
+
+        if (message.length < 5) {
+            alert('Message cant be blank!')
+            return
+        }
         //irasyti i db
+        try {
+            const docRef = await addDoc(collection(db, "contacts"), {
+                client_name: name,
+                client_email: email,
+                client_message: message,
+                created: new Date()
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.log(error)
+        }
         //isvalyti laukus
+
     }
 
     return (
@@ -24,8 +53,8 @@ const ContacsForm = () => {
                 <Form.Control
                     type="text"
                     placeholder="Enter your name"
-                    value={name} 
-                    onChange={(e)=> setName(e.target.value)}/>
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email address</Form.Label>
@@ -33,7 +62,7 @@ const ContacsForm = () => {
                     type="email"
                     placeholder="Enter your email"
                     value={email}
-                    onChange={(e)=> setEmail(e.target.value)}/>
+                    onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="forText">
                 <Form.Label>Question</Form.Label>
@@ -41,8 +70,8 @@ const ContacsForm = () => {
                     as="textarea"
                     placeholder="Enter your email"
                     value={message}
-                    onChange={(e)=> setMessage(e.target.value)}
-                     />
+                    onChange={(e) => setMessage(e.target.value)}
+                />
             </Form.Group>
 
 
